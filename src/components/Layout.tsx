788c6461d407f21950b4ceb386eb8770592e2be7
AndreDizon <cardizon.student@ua.edu.ph>
2026-04-25 20:04:06 +0800

    Optimize header layout - remove extra vertical space from theme toggle

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -34,7 +34,6 @@ export default function Layout({ children }: LayoutProps) {
     <div className="layout">
       <header className="main-header">
         <div className="header-top">
-          <div></div>
           <button 
             onClick={toggleTheme} 
             className="theme-toggle"
